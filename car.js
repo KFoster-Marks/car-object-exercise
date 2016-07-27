946009278ddbff5718d8664fb8ceb27694764b08
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-07-27 09:31:03 -0700

    done

--- car.js
+++ car.js
@@ -1,9 +1,32 @@
-
+'use strict';
 
 var Car = function (mpg) {
+  this.gallons = 0;
+  this.totalMiles = 0;
+  this.allTrips = [];
+};
+
+Car.prototype.gallons = function(){
+  console.log(this.gallons);
+  return this.gallons;
 };
 
 Car.prototype.fill = function (gallons) {
+  this.gallons += gallons;
+};
+
+Car.prototype.drive = function(miles) {
+  this.gallons = this.gallons - (miles/10);
+  this.totalMiles += miles;
+  this.allTrips.push(miles + " miles");
+};
+
+Car.prototype.odometer = function() {
+  return this.totalMiles;
+};
+
+Car.prototype.trips = function() {
+  return this.allTrips;
 };
 
 module.exports = Car;
